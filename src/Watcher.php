761ffa1b3d028f3c96352f3e45a833a599f4b840
sebaperez <sebalanus@gmail.com>
2016-04-01 10:20:48 -0400

    Show results formatted as mysql console output

--- src/Watcher.php
+++ src/Watcher.php
@@ -8,6 +8,7 @@ class Watcher {
 		$this->db = $db;
 		$this->server = $server;
 
+		$this->result_md5 = "";
 		$this->result = "";
 	}
 	public function watch($query) {
@@ -41,29 +42,80 @@ class Watcher {
 		$st->data_seek(0);
 		return $st;
 	}
+	private function getFormattedResponse($st) {
+		$sf = "";
+		$sizes = [];
+		$row = $st->fetch_assoc();
+		foreach ($row as $key => $value) {
+			$sizes[$key] = strlen($key);
+		}
+		while ($row = $st->fetch_assoc()) {
+			foreach ($row as $key => $value) {
+				$length = strlen($value);
+				if ($length > $sizes[$key]) {
+					$sizes[$key] = $length;
+				}
+			}
+		}
+		$st->data_seek(0);
+
+		foreach ($sizes as $length) {
+			$sf .= "+" . str_pad("", $length + 2, "-");
+		}
+		$sf .= "+\n";
+		
+		$row = $st->fetch_assoc();
+		foreach ($row as $key => $value) {
+			$sf .= "| ";
+			$sf .= str_pad($key, $sizes[$key] + 1);
+		}
+		$sf .= "|\n";
+
+		foreach ($sizes as $length) {
+			$sf .= "+" . str_pad("", $length + 2, "-");
+		}
+		$sf .= "+\n";
+
+		do {
+			foreach ($row as $key => $value) {
+				$sf .= "| ";
+				$sf .= str_pad($value, $sizes[$key] + 1);
+			}
+			$sf .= "|\n";
+		} while ($row = $st->fetch_assoc());
+
+		foreach ($sizes as $length) {
+			$sf .= "+" . str_pad("", $length + 2, "-");
+		}
+		$sf .= "+\n";
+
+		$st->data_seek(0);
+		return $sf;
+	}
 	private function outdated() {
 		$st = $this->_query($this->query);
 		$s = "";
+		$sf = $this->getFormattedResponse($st);
 		while ($row = $st->fetch_assoc()) {
 			$s .= implode("", $row);
 		}
 		$s = md5($s);
-		if ($this->result != $s) {
-			$this->result = $s;
-			return true;
+		if ($this->result_md5 != $s) {
+			$this->result_md5 = $s;
+			return $sf;
 		} else {
 			return false;
 		}
 	}
-	private function update() {
+	private function update($sf) {
 		passthru('clear');
 		$this->out("Watching: " . $this->query . "\n\n");
-		passthru("mysql -e '" . $this->query . "' " . $this->db);
+		$this->out($sf);
 	}
 	private function _watch() {
 		while(1) {
-			if ($this->outdated()) {
-				$this->update();
+			if ($sf = $this->outdated()) {
+				$this->update($sf);
 			}
 			sleep(1);
 		}
